a2d6d47e13bdbeef67cb57a7f39259dd9f7cd7c6
Jacob Hill <jacobhill.mail@gmail.com>
2019-03-06 11:33:52 -0800

    refactor harvest round two

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ DLME Scripts for harvesting data from providers
 ## oai-harvest.py
 
 OAI-PMH collections can be harvested with `oai-harvest.py` by calling the script followed by the base url of the sources you wish to harvest, the metadata prefix, and the name of the institution.
-
-`python oai-harvest.py http://cdm21044.contentdm.oclc.org/oai/oai.php -m oai_dc`
-
+```
+python oai-harvest.py http://cdm21044.contentdm.oclc.org/oai/oai.php -m oai_dc
+```
 Files will be written to `output/data`,
 
 ### Optional flags
-* `-s` If `-s TRUE` is called, the script will grab available sets first and files will be written to `output/set_name/data`. 
+* `-s` If `-s ` is called, the script will grab available sets first and files will be written to `output/set_name/data`. 
 * `-i` If the `-i institution_name` is called, the script will look in the `do_not_harvest` dictionary in `helper.py` for the name of the institution and pass along sets to ignore if any are found.
 
 # Harvest Server 
--- harvest-refactor/oai-harvest.py
+++ harvest-refactor/oai-harvest.py
@@ -6,26 +6,6 @@ from sickle import Sickle
 from sickle.iterator import OAIResponseIterator
 
 def main():
-    # CLI client options.
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-i",
-        "--institution",
-        dest="institution",
-        help="add the institution and the set names to ignore to the helper.do_not_harvest dictionary")
-    parser.add_argument("-m", "--metadata_prefix", dest="metadata_prefix",
-                        help="add the metadata prefix if other than oai_dc")
-    parser.add_argument(
-        "-s",
-        "--sets",
-        dest="sets",
-        help="add the -s flag if sets are a feature used by the data provider")
-    parser.add_argument(
-        "base_url",
-        help="put the base url you want harvested here.")
-
-    args = parser.parse_args()
-
     # Create sickle instance
     sickle = Sickle(args.base_url)
 
@@ -40,16 +20,33 @@ def main():
                     set=s.setSpec,
                     ignore_deleted=True)
                 file_path = 'output/{}/data/'.format(s.setSpec)
-                for record in records:
-                    helper.write_records(records, file_path)
+                # for record in records:
+                helper.write_records(records, file_path)
 
     else:
         records = sickle.ListRecords(
             metadataPrefix=args.metadata_prefix,
             ignore_deleted=True)
-        print("Records created.")  # status update
         file_path = 'output/data/'
         helper.write_records(records, file_path)
 
 if __name__ == "__main__":
+    # CLI client options.
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--institution",
+        help="add the institution and the set names to ignore to the helper.do_not_harvest dictionary")
+    parser.add_argument("-m", "--metadata_prefix",
+                        help="add the metadata prefix if other than oai_dc")
+    parser.add_argument(
+        "-s",
+        "--sets",
+        action='store_true',
+        help="add the -s flag if sets are a feature used by the data provider")
+    parser.add_argument(
+        "base_url",
+        help="put the base url you want harvested here.")
+
+    args = parser.parse_args()
     main()
