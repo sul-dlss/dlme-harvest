92a987b14a66d472ca5695f0745a9f75d0fcb73c
Jacob Hill <jacobhill.mail@gmail.com>
2019-11-21 09:53:53 -0500

    revise AUC harvest script

--- harvest-refactor/auc-harvest.py
+++ harvest-refactor/auc-harvest.py
@@ -9,16 +9,29 @@ def main():
                       'p15795coll22', 'p15795coll25']
 
     sickle = Sickle("http://cdm15795.contentdm.oclc.org/oai/oai.php")
-    sets = sickle.ListSets()
-    for s in sets:
-        if s.setSpec not in do_not_harvest:
-            directory = "output/auc/{}/data/".format(s.setSpec)
-            os.makedirs(os.path.dirname(directory), exist_ok=True)
 
-            records = sickle.ListRecords(metadataPrefix='oai_dc', set=s.setSpec, ignore_deleted=True)
-            for counter, record in enumerate(records, start=1):
-                with open('{}{}-{}.xml'.format(directory, s.setSpec, counter), 'w') as f:
-                    f.write(record.raw)
+    # Harvest specific listed sets
+    specific_sets = ['sphinx']
+    for s in specific_sets:
+        directory = "output/auc/{}/data/".format(s)
+        os.makedirs(os.path.dirname(directory), exist_ok=True)
+
+        records = sickle.ListRecords(metadataPrefix='oai_dc', set=s, ignore_deleted=True)
+        for counter, record in enumerate(records, start=1):
+            with open('{}{}-{}.xml'.format(directory, s, counter), 'w') as f:
+                f.write(record.raw)
+
+    # Or, harvest everything not in do_not_harvest
+    # sets = sickle.ListSets()
+    # for s in sets:
+    #     if s.setSpec not in do_not_harvest:
+    #         directory = "output/auc/{}/data/".format(s.setSpec)
+    #         os.makedirs(os.path.dirname(directory), exist_ok=True)
+    #
+    #         records = sickle.ListRecords(metadataPrefix='oai_dc', set=s.setSpec, ignore_deleted=True)
+    #         for counter, record in enumerate(records, start=1):
+    #             with open('{}{}-{}.xml'.format(directory, s.setSpec, counter), 'w') as f:
+    #                 f.write(record.raw)
 
 if __name__ == "__main__":
     main()
