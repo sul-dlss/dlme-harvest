1cd2e276647f80e98de4aae5fff50281b56a09df
Jacob Hill <jacobhill.mail@gmail.com>
2020-02-06 10:11:17 -0500

    qnl refactor

--- harvest-refactor/qnl-harvest.py
+++ harvest-refactor/qnl-harvest.py
@@ -9,14 +9,14 @@ base_output_folder = 'output'
 sickle = Sickle('https://api.qdl.qa/oaipmh')
 print("Sickle instance created.") # status update
 
-records = sickle.ListRecords(metadataPrefix='mods_no_ocr', ignore_deleted=True, resumptionToken='5603')
+records = sickle.ListRecords(metadataPrefix='mods', ignore_deleted=True)
 print("Records created.") # status update
 
 directory = "output/qnl/data/"
 os.makedirs(os.path.dirname(directory), exist_ok=True)
 
 # Change start to the resumption token plus 1
-for count, record in enumerate(records, start=5604):
+for count, record in enumerate(records, start=1):
     try:
         print("Record number " + str(count))
         out_file = 'output/qnl/data/qnl-{}.xml'.format(count)
