20f9c49b379adad152c47038add9cbc1198a0094
Jacob Hill <jacobhill.mail@gmail.com>
2020-02-06 00:34:46 -0500

    Revise qnl script

--- harvest-refactor/qnl-harvest.py
+++ harvest-refactor/qnl-harvest.py
@@ -16,15 +16,11 @@ directory = "output/qnl/data/"
 os.makedirs(os.path.dirname(directory), exist_ok=True)
 
 for count, record in enumerate(records, start=1):
-    if record.resumption_token is None:
-        print("None type found {}".format(count))
-        record._next_response()
-    else:
-        try:
-            print("Record number " + str(count))
-            out_file = 'output/qnl/data/qnl-{}.xml'.format(count)
-            directory_name = os.path.dirname(out_file)
-            with open(out_file, 'w') as f:
-            	f.write(record.raw)
-        except Exception as err:
-            print(err)
+    try:
+        print("Record number " + str(count))
+        out_file = 'output/qnl/data/qnl-{}.xml'.format(count)
+        directory_name = os.path.dirname(out_file)
+        with open(out_file, 'w') as f:
+        	f.write(record.raw)
+    except Exception as err:
+        print(err)
