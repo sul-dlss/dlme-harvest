2162b152b352c961f67e120727f4dbb96b4d10cd
Jacob Hill <jacobhill.mail@gmail.com>
2021-01-22 14:34:29 -0500

    Update analysis scripts

--- analysis-scripts/input-fields.py
+++ analysis-scripts/input-fields.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import json
+import json, os
 import pandas as pd
 from argparse import ArgumentParser
 from lxml import etree
@@ -9,19 +9,18 @@ def main():
     json_keys_with_values = []
     csv_col_names_with_values = []
     xml_elements_with_values = []
-
     # Main loop
-    for in_file in args.file:
+    for file in os.listdir(args.file[0]):
         # csv files
-        if in_file.endswith('.csv'):
-            df = pd.read_csv(in_file)
+        if file.endswith('.csv'):
+            df = pd.read_csv("{}{}".format(args.file[0], file))
             for col_name in df.columns:
                 csv_col_names_with_values.append(col_name)
 
         # json files
-        elif in_file.endswith('.json'):
+        elif file.endswith('.json'):
 
-            with open(in_file) as f:
+            with open("{}{}".format(args.file[0], file)) as f:
                 data = json.load(f)
                 # Will catch nested key, value pairs to the third level
                 for (k, v) in data.items():
@@ -37,23 +36,24 @@ def main():
                                                 json_keys_with_values.append("{}.{}.{}".format(k, k_1, k_2))
 
         # xml files
-        else:
-            tree = etree.parse(in_file)
-            root = tree.getroot()
-            events = ('start', 'end')
-            context = etree.iterparse(in_file, events=events)
-            for action, elem in context:
-                if elem.text:
-                    xml_elements_with_values.append(elem)
+        elif file.endswith('.xml'):
+                tree = etree.parse("{}{}".format(args.file[0], file))
+                root = tree.getroot()
+                events = ('start', 'end')
+                for event, element in etree.iterparse("{}{}".format(args.file[0], file)):
+                # context = etree.iterparse("{}{}".format(args.file[0], file), events=events)
+                # for action, elem in context:
+                    if element.text:
+                        xml_elements_with_values.append(element.tag)
 
-    # Print results
-    if args.file[0].endswith('.csv'):
-        print(set(csv_col_names_with_values))
-    elif args.file[0].endswith('.json'):
-        print(set(json_keys_with_values))
-    else:
-        for elem in set(xml_elements_with_values):
-            print(elem)
+        # Print results
+        # if args.file[0].endswith('.csv'):
+    print(set(csv_col_names_with_values))
+        # elif args.file[0].endswith('.json'):
+    print(set(json_keys_with_values))
+        # else:
+    for elem in set(xml_elements_with_values):
+        print(elem)
 
 if __name__ == '__main__':
     # CLI client options.
