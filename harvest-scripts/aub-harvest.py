53368a90bc8c4e8f91484d3e290e529f854a256d
Jacob Hill <jacobhill.mail@gmail.com>
2021-03-29 11:51:40 -0400

    Update AUB harvest script

--- harvest-scripts/aub-harvest.py
+++ harvest-scripts/aub-harvest.py
@@ -5,12 +5,12 @@ from sickle.iterator import OAIResponseIterator
 
 def main():
     sickle = Sickle("https://libraries.aub.edu.lb/xtf/oai")
-    sets = ['al-adab', 'postcards']
+    sets = ['aco', 'aladab', 'postcards', 'posters', 'travelbooks']
     for s in sets:
         directory = "output/aub/{}/data/".format(s)
         os.makedirs(os.path.dirname(directory), exist_ok=True)
 
-        records = sickle.ListRecords(metadataPrefix='oai_dc', set='postcards', ignore_deleted=True)
+        records = sickle.ListRecords(metadataPrefix='oai_dc', set=s, ignore_deleted=True)
         for counter, record in enumerate(records, start=1):
             with open('{}{}-{}.xml'.format(directory, s, counter), 'w') as f:
                 f.write(record.raw)
